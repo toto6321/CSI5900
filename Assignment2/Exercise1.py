b57168026ffb59d28910d679d628cdcdcf711b37
toto <toto6321@qq.com>
2019-02-18 00:25:18 -0500

    REFACTOR(a2): encapsulate the neural network model

--- Assignment2/Exercise1.py
+++ Assignment2/Exercise1.py
@@ -5,50 +5,53 @@ import numpy as np
 import torch
 from torch.autograd import Variable
 
-N = 100  # number of points per class
-D = 2  # dimensionality
-K = 3  # number of classes
-X = np.zeros((N * K, D))  # data matrix (each row = single example)
-y = np.zeros(N * K, dtype='uint8')  # class labels
-for j in range(K):
-    ix = range(N * j, N * (j + 1))
-    r = np.linspace(0.0, 1, N)  # radius
-    t = np.linspace(j * 4, (j + 1) * 4, N) + np.random.randn(N) * 0.2  # theta
-    X[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
-    y[ix] = j
 
-# lets visualize the data:
-plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
-plt.show()
+def exercise1(learning_rate=0.0001, n_hidden_layers=50, n_iterations=100_000):
+    """
+    :param learning_rate: the learning rate
+    :param n_hidden_layers: the number of the hidden layers
+    :param n_iterations: the number of hte iterations for gradient descent
+    :return: a numpy array of loss values over each gradient descent
+    """
 
-# generate hyper-parameters ranging from 0.1 to 10^-9
-N_RATES = 9
-learning_rates = [math.pow(10, -n) for n in range(1, 1 + N_RATES)]
+    N = 100  # number of points per class
+    D = 2  # dimensionality
+    K = 3  # number of classes
+    X = np.zeros((N * K, D))  # data matrix (each row = single example)
+    y = np.zeros(N * K, dtype='uint8')  # class labels
+    for j in range(K):
+        ix = range(N * j, N * (j + 1))
+        r = np.linspace(0.0, 1, N)  # radius
+        t = np.linspace(j * 4, (j + 1) * 4, N) + np.random.randn(N) * 0.2  # theta
+        X[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
+        y[ix] = j
 
-# define loss value record
-N_ITERATIONS = 20_000
-loss_array = np.zeros((N_ITERATIONS, N_RATES))
+    # lets visualize the data:
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
+    plt.show()
 
-dtype = torch.FloatTensor
-# augment input vectors
-x1 = Variable(torch.from_numpy(X).type(dtype), requires_grad=False)
-el1 = torch.cat((torch.ones(100), torch.zeros(200)), 0)
-el2 = torch.cat((torch.zeros(100), torch.ones(100), torch.zeros(100)), 0)
-el3 = torch.cat((torch.zeros(200), torch.ones(100)), 0)
-yy = torch.stack((el1, el2, el3), 1)
-y1 = Variable(torch.stack((el1, el2, el3), 1), requires_grad=False)
-h = 50  # size of hidden layer
+    # define loss value record
+    loss_values = np.zeros(n_iterations)
 
-for j in range(N_RATES):
+    dtype = torch.FloatTensor
+
+    # argument input vectors
+    x1 = Variable(torch.from_numpy(X).type(dtype), requires_grad=False)
+    el1 = torch.cat((torch.ones(100), torch.zeros(200)), 0)
+    el2 = torch.cat((torch.zeros(100), torch.ones(100), torch.zeros(100)), 0)
+    el3 = torch.cat((torch.zeros(200), torch.ones(100)), 0)
+    yy = torch.stack((el1, el2, el3), 1)
+    y1 = Variable(torch.stack((el1, el2, el3), 1), requires_grad=False)
+
     # parameters
-    w1 = Variable(0.5 * torch.randn(D, h), requires_grad=True)
-    b1 = Variable(torch.randn(1, h), requires_grad=True)
-    w2 = Variable(0.5 * torch.randn(h, K), requires_grad=True)
+    w1 = Variable(0.5 * torch.randn(D, n_hidden_layers), requires_grad=True)
+    b1 = Variable(torch.randn(1, n_hidden_layers), requires_grad=True)
+    w2 = Variable(0.5 * torch.randn(n_hidden_layers, K), requires_grad=True)
     b2 = Variable(torch.randn((1, K)), requires_grad=True)
 
     # run with different learning rates
     # gradient descent loop
-    for i in range(N_ITERATIONS):
+    for i in range(n_iterations):
 
         # Forward pass
         hout = x1.mm(w1) + b1
@@ -59,18 +62,18 @@ for j in range(N_RATES):
 
         # compute the loss
         loss = (scores - y1).pow(2).sum()
-        loss_array[i, j] = loss
+        loss_values[i] = loss
 
         loss.backward()
 
         if i % 10_000 == 0:
-            print(j, i, loss)
+            print(i, loss)
 
         # perform a parameter update
-        w2.data -= learning_rates[j] * w2.grad.data
-        w1.data -= learning_rates[j] * w1.grad.data
-        b2.data -= learning_rates[j] * b2.grad.data
-        b1.data -= learning_rates[j] * b1.grad.data
+        w2.data -= learning_rate * w2.grad.data
+        w1.data -= learning_rate * w1.grad.data
+        b2.data -= learning_rate * b2.grad.data
+        b1.data -= learning_rate * b1.grad.data
 
         # Manually zero the gradient after the backward pass
         w1.grad.data.zero_()
@@ -94,21 +97,38 @@ for j in range(N_RATES):
     error = N * K - torch.sum(err)
     print('The number of misclassified examples: ', error)
 
+    return loss_values
+
+
+# generate hyper-parameters ranging from 0.1 to 10^-9
+N_RATES = 9
+N_ITERATIONS = 20_000
+N_HIDDEN_LAYERS = 50
+learning_rates = [math.pow(10, -n) for n in range(1, 1 + N_RATES)]
+loss_array = np.empty((N_RATES, N_ITERATIONS))
+for i in range(N_RATES):
+    loss_array[i] = exercise1(
+        learning_rate=learning_rates[i],
+        n_hidden_layers=N_HIDDEN_LAYERS,
+        n_iterations=N_ITERATIONS
+    )
+
 # print the first 10 loss values
-for i in range(10):
+N_FIRST_ROWS = 10
+for i in range(N_FIRST_ROWS):
     for j in range(N_RATES):
-        print('{0: 10.3f}\t'.format(loss_array[i, j]), end='')
-    print('')
+        print('{0: 10.3f}\t'.format(loss_array[j, i]), end='')
+    print("")
+
 
 # plot the loss values
 figure2 = plt.figure()
 plot_x = [n for n in range(1, N_ITERATIONS + 1)]
 for i in range(N_RATES):
-    plt.plot(plot_x, loss_array[:, i])
+    plt.plot(plot_x, loss_array[i, :])
 
 plt.xlabel('iteration number')
 plt.ylabel('loss value')
-legend = ['learning rate is {}'.format(rate) for rate in learning_rates]
-plt.legend(legend, loc='upper right')
+plt.legend(['learning rate = {}'.format(learning_rate) for learning_rate in learning_rates], loc='upper right')
 plt.ylim(0, 500.0)
 plt.show()
